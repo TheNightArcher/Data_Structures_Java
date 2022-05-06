fb27e427d548836a5c10c47721145bf6b3b44ff5
Galin Manov <thenightarchervclaymore@gmail.com>
2022-05-06 23:18:57 +0300

    Working on HeapTree implementaion

--- 5. Heaps, BST/src/main/java/implementations/MaxHeap.java
+++ 5. Heaps, BST/src/main/java/implementations/MaxHeap.java
@@ -7,19 +7,47 @@ import java.util.Collections;
 import java.util.List;
 
 public class MaxHeap<E extends Comparable<E>> implements Heap<E> {
+    private List<E> elements;
+
+    public MaxHeap() {
+        this.elements = new ArrayList<>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
+        this.elements.add(element);
+        this.heapifyUp(this.size() - 1);
+    }
 
+    private void heapifyUp(int i) {
+        while (i >= 0 && isLess(getParentIndex(i), i)) {
+            Collections.swap(this.elements, getParentIndex(i), i);
+            i = getParentIndex(i);
+        }
+    }
+
+    private boolean isLess(int parentIndex, int childIndex) {
+        return getAt(parentIndex).compareTo(getAt(childIndex)) < 0;
+    }
+
+    private E getAt(int i) {
+        return this.elements.get(i);
+    }
+
+    private int getParentIndex(int i) {
+        return (i - 1) / 2;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.elements.isEmpty()) {
+            throw new IllegalStateException();
+        }
+        return getAt(0);
     }
 }
