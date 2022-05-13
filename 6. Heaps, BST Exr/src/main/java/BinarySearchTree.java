1fb4aecac668c842369d41a38c3a487f6d0e89cf
Galin Manov <thenightarchervclaymore@gmail.com>
2022-05-13 23:05:34 +0300

    init

--- 6. Heaps, BST Exr/src/main/java/BinarySearchTree.java
+++ 6. Heaps, BST Exr/src/main/java/BinarySearchTree.java
@@ -1,5 +1,8 @@
 import solutions.BinaryTree;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.function.Consumer;
 
 import java.util.List;
@@ -11,9 +14,11 @@ public class BinarySearchTree<E extends Comparable<E>> {
         private E value;
         private Node<E> leftChild;
         private Node<E> rightChild;
+        private int count;
 
-		public Node(E value) {
+        public Node(E value) {
             this.value = value;
+            this.count = 1;
         }
 
         public Node<E> getLeft() {
@@ -27,50 +32,221 @@ public class BinarySearchTree<E extends Comparable<E>> {
         public E getValue() {
             return this.value;
         }
+
+        public int getCount() {
+            return this.count;
+        }
     }
-	
-	public void eachInOrder(Consumer<E> consumer) {
-            
+
+    public BinarySearchTree() {
+    }
+
+    public BinarySearchTree(Node<E> root) {
+        this.root = root;
+    }
+
+    public void eachInOrder(Consumer<E> consumer) {
+
+        nodeInOrder(this.root, consumer);
     }
 
     public Node<E> getRoot() {
-        return null;
+        return this.root;
     }
 
     public void insert(E element) {
+        Node<E> newNode = new Node<>(element);
 
+        if (this.root == null) {
+            this.root = newNode;
+        } else {
+            insertInto(this.root, element);
+        }
     }
 
     public boolean contains(E element) {
+        Node<E> current = this.root;
+
+        while (current != null) {
+            if (isLess(element, current)) {
+                current = current.leftChild;
+            } else if (isGreater(element, current)) {
+                current = current.rightChild;
+            } else if (isEqual(element, current)) {
+                return true;
+            }
+        }
+
         return false;
     }
-    public BinarySearchTree<E> search(E element) {
-        return null;
-    }
-    public List<E> range(E first, E second) {
-      return null;
-    }
-    public void deleteMin() {
 
+    public BinarySearchTree<E> search(E element) {
+        BinarySearchTree<E> result = new BinarySearchTree<>();
+        Node<E> current = this.root;
+
+        while (current != null) {
+            if (isLess(element, current)) {
+                current = current.leftChild;
+            } else if (isGreater(element, current)) {
+                current = current.rightChild;
+            } else if (isEqual(element, current)) {
+                return new BinarySearchTree<>();
+            }
+        }
+
+        return result;
+    }
+
+    public List<E> range(E first, E second) {
+        List<E> result = new ArrayList<>();
+
+        Deque<Node<E>> deque = new ArrayDeque<>();
+        deque.offer(this.root);
+
+        while (!deque.isEmpty()) {
+            Node<E> current = deque.poll();
+
+            if (current.getLeft() != null) {
+                deque.offer(current.leftChild);
+            }
+
+            if (current.getRight() != null) {
+                deque.offer(current.rightChild);
+            }
+
+            if (isGreater(first, current) && isLess(second, current)) {
+                result.add(current.getValue());
+            } else if (isEqual(first, current) || isEqual(second, current)) {
+                result.add(current.getValue());
+            }
+        }
+
+        return result;
+    }
+
+    public void deleteMin() {
+        ensureNonEmpty();
+
+        if (this.root.getLeft() == null) {
+            this.root = this.root.getRight();
+            return;
+        }
+
+        Node<E> current = this.root;
+
+        while (current.getLeft().getLeft() != null) {
+            current.count--;
+            current = current.getLeft();
+        }
+
+        current.count--;
+        current.leftChild = current.getLeft().getRight();
     }
 
     public void deleteMax() {
+        ensureNonEmpty();
 
+        if (this.root.getRight() == null) {
+            this.root = this.root.getLeft();
+            return;
+        }
+
+        Node<E> current = this.root;
+
+        while (current.getRight().getRight() != null) {
+            current.count--;
+            current = current.getRight();
+        }
+
+        current.count--;
+        current.rightChild = current.getRight().getLeft();
     }
 
     public int count() {
-        return 0;
+        return this.root == null ? 0 : this.root.count;
     }
 
     public int rank(E element) {
-        return 0;
+        return nodeRank(this.root, element);
+    }
+
+    private int nodeRank(Node<E> node, E element) {
+        if (node == null) {
+            return 0;
+        }
+
+        if (isLess(element, node)) {
+            return nodeRank(node.getLeft(), element);
+        } else if (isEqual(element, node)) {
+            return getNodeCount(node.getLeft());
+        }
+
+        return getNodeCount(node.getLeft()) + 1 + nodeRank(node.getRight(), element);
     }
 
     public E ceil(E element) {
-        return null;
+        if (this.root == null) {
+            return null;
+        }
+
+        E nearestValue = null;
+
+
+        return nearestValue;
     }
 
     public E floor(E element) {
         return null;
+    }
+
+    private void nodeInOrder(Node<E> node, Consumer<E> consumer) {
+        if (this.root == null) {
+            return;
+        }
+
+        nodeInOrder(node.getLeft(), consumer);
+        consumer.accept(node.value);
+        nodeInOrder(node.getRight(), consumer);
+    }
+
+    private boolean isLess(E element, Node<E> node) {
+        return element.compareTo(node.getValue()) < 0;
+    }
+
+    private boolean isGreater(E element, Node<E> node) {
+        return element.compareTo(node.getValue()) > 0;
+    }
+
+    private boolean isEqual(E element, Node<E> node) {
+        return element.compareTo(node.getValue()) == 0;
+    }
+
+    private void insertInto(Node<E> node, E element) {
+        if (isGreater(element, node)) {
+            if (node.getRight() == null) {
+                node.rightChild = new Node<>(element);
+            } else {
+
+                insertInto(node.getRight(), element);
+            }
+        } else if (isLess(element, node)) {
+            if (node.getLeft() == null) {
+                node.leftChild = new Node<>(element);
+            } else {
+
+                insertInto(node.getLeft(), element);
+            }
+        }
+        node.count++;
+    }
+
+    private void ensureNonEmpty() {
+        if (this.root == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int getNodeCount(Node<E> node) {
+        return node == null ? 0 : node.getCount();
     }
 }
