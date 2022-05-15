0d214eea705844dc78aa076c62ba9d952850cae1
Galin Manov <thenightarchervclaymore@gmail.com>
2022-05-15 16:06:19 +0300

    init

--- 6. Heaps, BST Exr/src/main/java/solutions/BinaryTree.java
+++ 6. Heaps, BST Exr/src/main/java/solutions/BinaryTree.java
@@ -1,18 +1,87 @@
 package solutions;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class BinaryTree {
+    private int value;
+    private BinaryTree left;
+    private BinaryTree right;
 
-    public BinaryTree(int key, BinaryTree first, BinaryTree second) {
-
+    public BinaryTree(int value, BinaryTree left, BinaryTree right) {
+        this.value = value;
+        this.left = left;
+        this.right = right;
     }
 
     public Integer findLowestCommonAncestor(int first, int second) {
-        return null;
+        List<Integer> firstPath = findPath(first);
+        List<Integer> secondPath = findPath(second);
+
+        int smallerSize = Math.min(firstPath.size(), secondPath.size());
+
+        int i = 0;
+        for (; i < smallerSize; i++) {
+            if (!firstPath.get(i).equals(secondPath.get(i))) {
+                break;
+            }
+        }
+        return firstPath.get(i - 1);
     }
 
     public List<Integer> topView() {
-        return null;
+        List<Integer> result = new ArrayList<>();
+
+        BinaryTree currentLeftSide = this;
+        while (currentLeftSide != null) {
+            result.add(currentLeftSide.value);
+            currentLeftSide = currentLeftSide.left;
+        }
+
+        BinaryTree currentRightSide = this;
+        int count = 0;
+
+        while (currentRightSide != null) {
+            if (count == 0){
+                currentRightSide = currentRightSide.right;
+            }
+            result.add(currentRightSide.value);
+            currentRightSide = currentRightSide.right;
+            count++;
+        }
+
+        return result;
+    }
+
+    private List<Integer> findPath(int element) {
+        List<Integer> result = new ArrayList<>();
+        findNodePath(this, element, result);
+
+        return result;
+    }
+
+    private boolean findNodePath(BinaryTree binaryTree, int element, List<Integer> currentPath) {
+        if (binaryTree == null) {
+            return false;
+        }
+
+        if (binaryTree.value == element) {
+            return true;
+        }
+
+        currentPath.add(binaryTree.value);
+
+        boolean leftResult = findNodePath(binaryTree.left, element, currentPath);
+        if (leftResult) {
+            return true;
+        }
+
+        boolean rightResult = findNodePath(binaryTree.right, element, currentPath);
+        if (rightResult) {
+            return true;
+        }
+
+        currentPath.remove(Integer.valueOf(binaryTree.value));
+        return false;
     }
 }
--- 6. Heaps, BST Exr/src/main/java/solutions/MinHeap.java
+++ 6. Heaps, BST Exr/src/main/java/solutions/MinHeap.java
@@ -3,30 +3,76 @@ package solutions;
 import interfaces.Decrease;
 import interfaces.Heap;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MinHeap<E extends Comparable<E> & Decrease<E>> implements Heap<E> {
+    private List<E> elements;
+
+    public MinHeap() {
+        this.elements = new ArrayList<>();
+    }
 
     @Override
     public int size() {
-        return 0;
+        return this.elements.size();
     }
 
     @Override
     public void add(E element) {
-
+        this.elements.add(element);
+        this.heapIfDown(this.size() - 1);
     }
 
     @Override
     public E peek() {
-        return null;
+        if (this.elements.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return getAt(0);
     }
 
     @Override
     public E poll() {
-        return null;
+        if (this.elements.isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        E currentElement = this.elements.get(0);
+        this.elements.remove(0);
+
+        return currentElement;
     }
 
     @Override
     public void decrease(E element) {
+        int indexOfElement = this.elements.indexOf(element);
 
+        E current = this.elements.get(indexOfElement);
+
+        current.decrease();
+
+        heapIfDown(indexOfElement);
+    }
+
+    private E getAt(int index) {
+        return this.elements.get(index);
+    }
+
+    private int getParentIndex(int i) {
+        return (i - 1) / 2;
+    }
+
+    private void heapIfDown(int i) {
+        while (i >= 0 && isGreater(getParentIndex(i), i)) {
+            Collections.swap(this.elements, getParentIndex(i), i);
+            i = getParentIndex(i);
+        }
+    }
+
+    private boolean isGreater(int parentIndex, int childIndex) {
+        return getAt(parentIndex).compareTo(getAt(childIndex)) > 0;
     }
 }
